f0f714cd125f25b742a858f1ce09803b1cba3121
mashoun <mashoun.me@gmail.com>
2023-08-19 19:45:18 +0300

    +redesign of the blogs section

--- assets/js/components/blogs-section.js
+++ assets/js/components/blogs-section.js
@@ -2,7 +2,8 @@ export default {
     template:
     /*html*/
     `
-    <section class="container d-flex flex-column gap-3">
+  
+    <!-- <section class="container d-flex flex-column gap-3">
       <div class="row">
         <div class="col-12">
           <h1 class="text-info cin fs-2">{{contact.bHeading}}</h1>
@@ -25,7 +26,37 @@ export default {
       <div class="row" id="show-more">
         <div class="col-12 d-flex justify-content-center"><a class="icon-link icon-link-hover no-styles text-heading" :href="utilities.env('/blogs')">check all blogs <i class="bi bi-arrow-right"></i></a></div>
       </div>
-    </section>
+    </section> -->
+
+      
+    <section class="container d-flex flex-column gap-3">
+      <div class="row">
+        <div class="col-12">
+          <h1 class="text-info cin fs-2">{{contact.bHeading}}</h1>
+          <p class="text-justify text-normal fs-small pop">{{contact.bTagline}}</p>
+        </div>
+      </div>
+      <div class="row g-3 justify-content-start">
+        <div v-for="b in filteredBlogs" :key="b" class="col-12 col-md-6 col-lg-6">
+        <a :href="b.url" class="no-styles">
+          <div class="d-flex flex-column gap-2 bg-glass rounded p-3">
+            <div class="d-flex align-items-center gap-3">
+              <img :src="b.thumbnail" :alt="b.title" width="80" height="80" class="object-fit-cover rounded-circle skeleton" >
+              <strong class="text-fade-3 text-info pop fs-6 m-0">{{b.title}}</strong>
+            </div>
+            <p style="min-height: 50px;" class="text-fade-3 text-normal m-0 fs-small">{{b.description}}</p>
+            <time class="text-heading fs-xsmall pop">{{utilities.timo(b.date)}}</time>
+            
+          </div>
+          </a>
+        </div>
+      </div>
+      <div class="row" id="show-more">
+        <div class="col-12 d-flex justify-content-center"><a class="icon-link icon-link-hover no-styles text-heading" :href="utilities.env('/blogs')">check all blogs <i class="bi bi-arrow-right"></i></a></div>
+      </div>
+    </section> 
+
+
     `,
     computed:{
       filteredBlogs(){
--- assets/js/components/services-section.js
+++ assets/js/components/services-section.js
@@ -6,7 +6,6 @@ export default {
       <div class="row">
         <div class="col-12">
           <h1 class="text-info cin fs-2">{{contact.sHeading}}</h1>
-          <!-- <p class="text-justify text-normal fs-small pop">{{contact.sTagline}}</p> -->
         </div>
       </div>
       <div class="row g-3 justify-content-start">
@@ -23,6 +22,26 @@ export default {
         </div>
       </div>
     </section>
+    
+    <!-- <section class="container d-flex flex-column gap-3">
+    <div class="row">
+      <div class="col-12">
+        <h1 class="text-info cin fs-2">{{contact.sHeading}}</h1>
+      </div>
+    </div>
+    <div class="row g-3 justify-content-start">
+      <div class="col-12 col-md-6 col-lg-6"  v-for="s in services" :key="s" >
+        <a :href="s.url" class="no-styles d-flex flex-column gap-2 bg-glass rounded p-3">
+          <div class="d-flex gap-3 align-items-center">
+            <img :src="s.thumbnail" :alt="s.title" width="100" height="100" class="object-fit-cover rounded-circle border skeleton" >
+            <strong class="text-info pop fs-5 m-0 text-uppercase">{{s.title}}</strong>
+          </div>
+          <p style="min-height: 50px;" class="text-fade-3 text-normal m-0 fs-small">{{s.description}}</p> 
+          <button class="btn btn-sm btn-outline-info">Know more about this service</button>
+        </a>
+      </div>
+    </div>
+  </section> -->
     `,
     props:['services','utilities','contact']
 }
